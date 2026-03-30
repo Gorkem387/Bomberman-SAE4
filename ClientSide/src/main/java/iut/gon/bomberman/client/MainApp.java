7705bbaf1d5719c2d2a7c4a32c555ce7b90fa155
Oryx87 <baudin.mylan@laposte.net>
2026-03-30 13:22:39 +0200

    Finalisation maquette 1er fenêtre

--- ClientSide/src/main/java/iut/gon/bomberman/client/MainApp.java
+++ ClientSide/src/main/java/iut/gon/bomberman/client/MainApp.java
@@ -1,4 +1,29 @@
 package iut.gon.bomberman.client;
 
-public class MainApp {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/launcher.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Bomberman - Menu Principal");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            System.err.println("Erreur lors du chargement de l'appliaction.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- ClientSide/src/main/java/module-info.java
+++ ClientSide/src/main/java/module-info.java
@@ -1,2 +1,11 @@
-module $MODULE_NAME$ {
+module ClientSide {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.fxml;
+
+    opens iut.gon.bomberman.client.controllers to javafx.fxml;
+
+    opens iut.gon.bomberman.client to javafx.fxml;
+
+    exports iut.gon.bomberman.client;
 }
