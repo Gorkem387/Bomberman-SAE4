34effebbdb39c6631a7820f8cf96339a54855e1e
Gorkem387 <gorkemyildiz7ds@gmail.com>
2026-03-30 13:36:35 +0200

    [FEAT] ajout méthode draw dans LabRenderer, et ajour assets de mur et wall

--- ClientSide/src/main/java/iut/gon/bomberman/client/view/LabRenderer.java
+++ ClientSide/src/main/java/iut/gon/bomberman/client/view/LabRenderer.java
@@ -3,7 +3,28 @@ package iut.gon.bomberman.client.view;
 import iut.gon.bomberman.common.model.labyrinthe.CellType;
 import iut.gon.bomberman.common.model.labyrinthe.Labyrinthe;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
 public class LabRenderer {
+    private static final int TILE_SIZE = 32;
+
+    private final Image wallImg = new Image(getClass().getResourceAsStream("/iut/gon/bomberman/client/assets/block_04.png"));
+    private final Image destructibleImg = new Image(getClass().getResourceAsStream("/iut/gon/bomberman/client/assets/block_06.png"));
+    private final Image groundImg = new Image(getClass().getResourceAsStream("/iut/gon/bomberman/client/assets/ground_01.png"));
+
+    public void draw(GraphicsContext gc, Labyrinthe lab) {
+        for (int x = 0; x < lab.getWidth(); x++) {
+            for (int y = 0; y < lab.getHeight(); y++) {
+                CellType type = lab.getCell(x,y);
+
+                gc.drawImage(groundImg, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                if (type == CellType.WALL) {
+                    gc.drawImage(wallImg, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                } else if (type == CellType.DESTRUCTIBLE) {
+                    gc.drawImage(destructibleImg, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                }
+            }
+        }
+    }
 }
