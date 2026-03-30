be187408e0ef00a42da55d939065f0bc0d4fe9a8
Akumaa <valoumar0711@gmail.com>
2026-03-30 12:49:29 +0200

    [FIX] pom.xml erreurs fix

--- bomberman/ServerSide/src/main/java/module-info.java
+++ bomberman/ServerSide/src/main/java/module-info.java
@@ -1,9 +1,3 @@
 module iut.gon.serverside {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-
-    opens iut.gon.serverside to javafx.fxml;
     exports iut.gon.serverside;
 }
