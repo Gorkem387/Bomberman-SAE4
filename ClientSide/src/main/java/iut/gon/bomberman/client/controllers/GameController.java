b2b8d8348758ca36cabf683cb9fd74df692f6424
Gorkem387 <gorkemyildiz7ds@gmail.com>
2026-03-30 14:26:07 +0200

    [FEAT] Ajout gameloop pour le jeu avec AnimationTimer

--- ClientSide/src/main/java/iut/gon/bomberman/client/controllers/GameController.java
+++ ClientSide/src/main/java/iut/gon/bomberman/client/controllers/GameController.java
@@ -5,6 +5,8 @@ import iut.gon.bomberman.common.model.labyrinthe.DFSGenerator;
 import iut.gon.bomberman.common.model.labyrinthe.Labyrinthe;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.animation.AnimationTimer;
+import javafx.scene.canvas.GraphicsContext;
 
 import javax.swing.*;
 
@@ -12,6 +14,7 @@ public class GameController {
     @FXML
     private Canvas gameCanvas;
 
+    private GraphicsContext gc;
     private final LabRenderer renderer = new LabRenderer();
     private Labyrinthe labyrinthe;
 
@@ -19,14 +22,30 @@ public class GameController {
     public void initialize() {
         DFSGenerator generator = new DFSGenerator();
         this.labyrinthe = generator.createLabyrinthe(21, 21);
+        this.gc = gameCanvas.getGraphicsContext2D();
 
         gameCanvas.setWidth(labyrinthe.getWidth() * 32);
         gameCanvas.setHeight(labyrinthe.getHeight() * 32);
 
-        draw();
+        // Game Loop
+        AnimationTimer gameLoop = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                // Cette méthode tourne à 60 FPS
+                update();
+                render();
+            }
+        };
+        gameLoop.start();
     }
-    
-    private void draw() {
-        renderer.draw(gameCanvas.getGraphicsContext2D(), labyrinthe);
+
+    private void update() {
+        // player update ici
+    }
+
+    private void render() {
+        gc.clearRect(0, 0, gameCanvas.getWidth(), gameCanvas.getHeight());
+        renderer.draw(gc, labyrinthe);
+        // TODO render player
     }
 }
